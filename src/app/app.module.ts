23ceea0e4a3ba4db34df0ee75d8ff7b4bae9893f
Allan Weisser Verdessi <vredes@live.com>
2021-08-24 00:26:13 -0400

    charts compatibility issues fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+//Graficos
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [ 
+    BrowserModule, 
+    ChartsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
