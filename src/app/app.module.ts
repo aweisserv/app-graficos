c1914aa91705e01f2928dfdab016fedf046698be
Allan Weisser Verdessi <vredes@live.com>
2021-08-25 01:39:33 -0400

    line chart logic fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 
 //Graficos
 import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent
   ],
   imports: [ 
     BrowserModule, 
